0565e97ba6c0e0e207814335c00cbc0b8b512b43
pradipbade563-ui <pradipbade563@gmail.com>
2026-05-01 16:10:06 +0530

    no changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,6 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 
-// import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-// export default defineConfig();
-
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: '/'
-})
+export default defineConfig();
